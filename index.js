2931833a1b9cf346724f72c15465db2aa4641707
sohag292 <sohagshah571@gmail.com>
2023-05-20 17:37:30 +0600

    toys findOne data implementd

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 2000;
 
@@ -27,12 +27,47 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
+    
+    const ToysCollection = client.db("ToySportzDB").collection("ToySport");
+
+
+    app.get('/addToy', async(req,res)=>{
+        const cursor = ToysCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+
+    })
+
+
+    app.get('/addToy/:id', async(req,res)=>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)}
+        const user = await ToysCollection.findOne(query);
+        res.send(user)
+
+    })
+ 
+      
+
+
+    app.post('/addToy', async(req,res)=>{
+        const newaddToy = req.body;
+        console.log(newaddToy);
+        const result = await ToysCollection.insertOne(newaddToy)
+        res.send(result);
+
+    })
+
+    
+
+
+
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
